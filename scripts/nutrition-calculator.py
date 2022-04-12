67b22662d8e0dac757e1b523225920c874168189
Antonio Pelayo <aPelayo.py@gmail.com>
2022-04-11 20:35:32 -0700

    init branch

---
+++ scripts/nutrition-calculator.py
@@ -0,0 +1,12 @@
+# Nutrition Calculator
+import pandas as pd
+
+
+if __name__ == '__main__':
+  print("Welcome to DDSC's Nutrition Calculator!\n")
+
+  # TODO: Get user input
+  dc = input("What dinning commons did you go to? ")
+
+  # TODO: Filter data
+  # TODO: Output results
